9d810cdd2c6ee5a36ccff7098842c1929799d3c1
wiki <wiki.peng@qq.com>
2015-07-22 17:43:48 +0800

    gameplayer game

---
+++ src/GameConfig.js
@@ -0,0 +1,5 @@
+var GameConfig=GameConfig||{};
+
+//游戏背景宽高
+GameConfig.BG_W = 750;
+GameConfig.BG_H = 1334;
--- src/resource.js
+++ src/resource.js
@@ -1,7 +1,11 @@
 var res = {
     HelloWorld_png : "res/HelloWorld.png",
     CloseNormal_png : "res/CloseNormal.png",
-    CloseSelected_png : "res/CloseSelected.png"
+    CloseSelected_png : "res/CloseSelected.png",
+    GameBg:"res/GameBg.png"
+    //,
+    //avatars_png:"res/avatars.png",
+    //avatars_plist:"res/avatars.plist"
 };
 
 var g_resources = [];
